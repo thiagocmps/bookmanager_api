730853dd05d36dd9f66132496eebdf19c1260dde
Thiago Campos Vidal de Souza <thiagocvs2003@gmail.com>
2025-01-28 18:31:15 +0000

    Refatora rotas de livros para melhorar a legibilidade e atualiza a rota de exclusão de livros

--- Routes/route_books.js
+++ Routes/route_books.js
@@ -22,7 +22,7 @@ bookRouter.get("/userbooks", async function (req, res) {
 });
 
 /* GET one by id */
- bookRouter.get("/:id", utilities.getBookById, async function (req, res) {
+bookRouter.get("/:id", utilities.getBookById, async function (req, res) {
   res.send(res.userbook);
 });
 
@@ -36,33 +36,41 @@ bookRouter.post("/createbook", async function (req, res) {
 });
 
 /* UPDATE userbook - Patch update just the field that the user changed */
- bookRouter.patch("/updateuserbookreview/:id", utilities.getBookById, async function (req, res) {
-  console.log("req.userbook: " + res.userbook)
-  console.log("res.userbook.review.rating: " + res.userbook.review.rating)
-  console.log("req.body.rating: " + req.body.review.rating)
-  res.userbook.review.rating = req.body.review.rating
-  res.userbook.review.description = req.body.review.description
+bookRouter.patch(
+  "/updateuserbookreview/:id",
+  utilities.getBookById,
+  async function (req, res) {
+    console.log("req.userbook: " + res.userbook);
+    console.log("res.userbook.review.rating: " + res.userbook.review.rating);
+    console.log("req.body.rating: " + req.body.review.rating);
+    res.userbook.review.rating = req.body.review.rating;
+    res.userbook.review.description = req.body.review.description;
 
-  try {
-    const updatedUserbook = await res.userbook.save()
-    res.json(updatedUserbook)
-  } catch (error) {
-    res.status(400).json({message: error.message})
+    try {
+      const updatedUserbook = await res.userbook.save();
+      res.json(updatedUserbook);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
   }
-});
+);
 
-bookRouter.patch("/updateuserbookstate/:id", utilities.getBookById, async function (req, res) {
-  res.userbook.state = req.body.state
-  try {
-    const updatedUserbook = await res.userbook.save()
-    res.json(updatedUserbook)
-  } catch (error) {
-    res.status(400).json({message: error.message})
+bookRouter.patch(
+  "/updateuserbookstate/:id",
+  utilities.getBookById,
+  async function (req, res) {
+    res.userbook.state = req.body.state;
+    try {
+      const updatedUserbook = await res.userbook.save();
+      res.json(updatedUserbook);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
   }
-});
+);
 
 /* DELETE userbook ADMIN */
-bookRouter.delete("/admin/:id", utilities.getBookById, utilities.isAdmin, async function (req, res) {
+/* bookRouter.delete("/admin/:id", utilities.getBookById, utilities.isAdmin, async function (req, res) {
     try {
       await res.userbook.deleteOne()
       res.json({ message: "Book deleted" });
@@ -70,17 +78,32 @@ bookRouter.delete("/admin/:id", utilities.getBookById, utilities.isAdmin, async 
       res.status(500).json({ message: error.message });
     }
   }
-);
+); */
+
+/* DELETE userbook ADIMIN */
+/* bookRouter.delete(
+  "/:id",
+  utilities.getBookById,
+  utilities.isAdmin,
+  async function (req, res) {
+    try {
+      await res.userbook.deleteOne();
+      res.json({ message: "Book deleted" });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+); */
 
 /* DELETE userbook */
-bookRouter.delete("/:id", utilities.getBookById, utilities.isAdmin, async function (req, res) {
+bookRouter.delete("/deleteuserbook/:id", utilities.getBookById, async function (req, res) {
   try {
-    await res.userbook.deleteOne()
+    await res.userbook.deleteOne();
+    console.log("Book deleted");
     res.json({ message: "Book deleted" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-}
-);
+});
 
 module.exports = bookRouter;
